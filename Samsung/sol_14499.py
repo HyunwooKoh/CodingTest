848f2ac664ba893d358fb41de2913f6d81132d95
hyunwookoh <trghyunwoo@gmail.com>
2023-08-24 08:07:40 +0900

    feat: 백준 - 삼성 - 14499- 주사위 굴리기 풀이 (2)
    주사위 굴리는 방향 점검 필요

--- Samsung/sol_14499.py
+++ Samsung/sol_14499.py
@@ -16,44 +16,44 @@ if __name__ == "__main__":
     orders = list(map(int, input().split()))
 
     for o in orders :
+        print(mat)
+        print(dice)
         nx = x + dx[o]
         ny = y + dy[o]
+        if 0<= nx < m and 0 <= ny < n:        
+            x , y = nx, ny
+            # move dice
+            if o == 3:
+                temp = dice[5]
+                dice[5] = dice[2]
+                dice[2] = dice[0]
+                dice[0] = dice[3]
+                dice[3] = temp
+            elif o == 4:
+                temp = dice[3]
+                dice[3] = dice[0]
+                dice[0] = dice[2]
+                dice[2] = dice[5]
+                dice[5] = temp
+            elif o == 1:
+                temp = dice[1]
+                dice[1] = dice[0]
+                dice[0] = dice[4]
+                dice[4] = dice[5]
+                dice[5] = temp
+            elif o == 2:
+                temp = dice[4]
+                dice[4] = dice[0]
+                dice[0] = dice[1]
+                dice[1] = dice[5]
+                dice[5] = temp
 
-        if nx < 0 or nx >= m or ny < 0 or ny >= n:
-            continue
-        x , y = nx, ny
-        
-        # move dice
-        if o == 3:
-            temp = dice[5]
-            dice[5] = dice[0]
-            dice[0] = dice[2]
-            dice[2] = dice[5]
-            dice[5] = temp
-        elif o == 4:
-            temp = dice[3]
-            dice[3] = dice[0]
-            dice[0] = dice[2]
-            dice[2] = dice[5]
-            dice[5] = temp
-        elif o == 1:
-            temp = dice[1]
-            dice[1] = dice[0]
-            dice[0] = dice[4]
-            dice[4] = dice[5]
-            dice[5] = temp
-        elif o == 2:
-            temp = dice[4]
-            dice[4] = dice[0]
-            dice[0] = dice[1]
-            dice[1] = dice[5]
-            dice[5] = temp
-        
-        # process logic
-        if mat[y][x] == 0:
-            mat[y][x] = dice[0]
-        else:
-            # copy mat[y][x] to bottom of dice
-            mat[y][x] = 0
 
-        print() # Top of Dice
+            # process logic
+            if mat[y][x] == 0:
+                mat[y][x] = dice[0]
+            else:
+                # copy mat[y][x] to bottom of dice
+                dice[0] = mat[y][x]
+                mat[y][x] = 0
+            print(dice[5]) # Top of Dice
