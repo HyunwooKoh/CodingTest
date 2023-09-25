04e859364638e7b0dab894db9019324d4d97ab2c
hyunwookoh <trghyunwoo@gmail.com>
2023-09-26 07:46:57 +0900

    feat: 삼성SW기출 - 2개의 사탕 문제 풀이 (3)
    최종 버전

--- codeTree/two-candies.py
+++ codeTree/two-candies.py
@@ -12,75 +12,57 @@ def bfs(r_x, r_y, b_x, b_y):
     res = -1
 
     while queue and res == -1:
-        rpx, rpy, bpx, bpy, cnt = queue.popleft()
-        if cnt >= 10:
+        rx, ry, bx, by, cnt = queue.popleft()
+        if cnt > 10:
             continue
 
-        print("POP")
-        print("rx : " + str(rpx) + ", ry : " + str(rpy))        
-        print("bx : " + str(bpx) + ", by : " + str(bpy))
-                
         for i in range(4):
             red, blue = False, False
             rCnt, bCnt = 0, 0
+            nrx, nry = rx, ry
+            nbx, nby = bx, by
             
             while True: # 빨간공 이동
-                nrx = rpx + dx[i]
-                nry = rpy + dy[i]
-                if i == 2:
-                    print(dx[i])
-                    print(dy[i])
-                    print(str(nrx) + " " + str(nry))
-                if nrx < 0 or nrx >= m or nry < 0 or nry >= n:
+                nrx += dx[i]
+                nry += dy[i]
+                rCnt += 1
+                if nrx < 0 or nrx >= m or nry < 0 or nry >= n or mat[nry][nrx] == '#':
+                    nrx -= dx[i]
+                    nry -= dy[i]
+                    rCnt -= 1
                     break
-
-                if mat[nry][nrx] == '#':
-                    break
-                else:
-                    rCnt += 1
-                    rpx = nrx
-                    rpy = nry
-                    if  mat[nry][nrx] == 'O':
+                elif  mat[nry][nrx] == 'O':
                         red = True
                         break
             
             while True: # 파란공 이동
-                nbx = bpx + dx[i]
-                nby = bpy + dy[i]
-                
-                if nbx < 0 or nbx >= m or nby < 0 or nby >= n:
+                nbx += dx[i]
+                nby += dy[i]
+                bCnt += 1
+                if nbx < 0 or nbx >= m or nby < 0 or nby >= n or mat[nby][nbx] == '#':
+                    nbx -= dx[i]
+                    nby -= dy[i]
+                    bCnt -= 1
                     break
-
-                if mat[nby][nbx] == '#':
-                    break
-                else:
-                    bCnt += 1
-                    bpx = nbx
-                    bpy = nby
-                    if mat[nby][nbx] == 'O':
+                elif mat[nby][nbx] == 'O':
                         blue = True
                         break
-            if i == 2:
-                print(i)
-                print("rx : " + str(rpx) + ", ry : " + str(rpy))        
-                print("bx : " + str(bpx) + ", by : " + str(bpy))
-                print()
                     
             if red and not blue: # 빨간 공만 나간 경우
                 res = cnt
                 break
             elif not red and not blue: # 둘 다 안나간 경우
-                if rpx == bpx and rpy == bpy:
+                if nrx == nbx and nry == nby:
                     if rCnt > bCnt: # 빨간 공이 뒤인 경우
-                        rpx -= dx[i]
-                        rpy -= dy[i]
+                        nrx -= dx[i]
+                        nry -= dy[i]
                     else:           # 파란 공이 뒤인 경우
-                        bpx -= dx[i]
-                        bpy -= dy[i]
-                if not rVisited[rpy][rpx] or not bVisited[bpy][bpx]:
-                    rVisited[rpy][rpx] = True
-                    bVisited[bpy][bpx] = True
-                    queue.append((rpx, rpy, bpx, bpy, cnt+1))
+                        nbx -= dx[i]
+                        nby -= dy[i]
+                
+                if (nrx, nry, nbx, nby) not in visited:
+                    queue.append((nrx, nry, nbx, nby, cnt+1))
+                    visited.append((nrx, nry, nbx, nby))
     return res
             
 
@@ -89,8 +71,7 @@ if __name__ == "__main__":
     rx, ry = -1, -1
     bx, by = -1, -1
     mat = []
-    rVisited = [[False for _ in range(m)] for _ in range(n)]
-    bVisited = [[False for _ in range(m)] for _ in range(n)]
+    visited = []
     for i in range(n):
         line = list(input())
         for j in range(m):
@@ -102,6 +83,5 @@ if __name__ == "__main__":
                 by = i
         mat.append(line)
     
-    rVisited[ry][rx] = True
-    bVisited[by][bx] = True
+    visited.append((rx,ry,bx,by))
     print(bfs(rx, ry, bx, by))
