9f17765353fe3b5cccfb61bf1dd4168057624c6d
hyunwoo.koh <trghyunwoo@gmail.com>
2023-10-04 13:56:48 +0900

    feat: 삼성SW기출 - 조삼모사 문제 풀이 (2)
    최종 버전

--- codeTree/three-at-dawn-and-four-at-dusk.py
+++ codeTree/three-at-dawn-and-four-at-dusk.py
@@ -1,6 +1,4 @@
-# 삼성 SW역량 테스트 기출문제 - 조삼모사
 # ref https://www.codetree.ai/training-field/frequent-problems/problems/three-at-dawn-and-four-at-dusk/
-
 import sys
 from itertools import combinations
 input = sys.stdin.readline
@@ -10,8 +8,24 @@ if __name__ == "__main__":
     nums = [i for i in range(n)]
     p = [list(map(int, input().split())) for _ in range(n)]
 
+    res = 1e9
     for comb in combinations(nums, n//2):
-        # comb -> 아침
-        # not comb -> 저녁
-        # 강도 개산 함수 구현
-        # 차이 값 확인 후 갱신
+        morning = list(comb)
+        night = []
+        for i in nums:
+            if i not in morning:
+                night.append(i)
+        
+        morningRes = 0
+        nightRes = 0
+        for i in range(len(morning)):
+            for j in range(i+1, len(morning)):
+                morningRes += p[morning[i]][morning[j]]
+                morningRes += p[morning[j]][morning[i]]
+        
+        for i in range(len(night)):
+            for j in range(i+1, len(night)):
+                nightRes += p[night[i]][night[j]]
+                nightRes += p[night[j]][night[i]]
+        res = min(abs(morningRes-nightRes), res)
+    print(res)
