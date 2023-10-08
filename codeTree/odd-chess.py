b7280cbf5a2c465a1ea742c418d93ad82c2fda93
hyunwookoh <trghyunwoo@gmail.com>
2023-10-08 22:51:04 +0900

    feat: 삼성SW기출 - 이상한 체스 문제 풀이 (2)
    최종 버전

--- codeTree/odd-chess.py
+++ codeTree/odd-chess.py
@@ -1,64 +1,62 @@
 # ref https://www.codetree.ai/training-field/frequent-problems/problems/odd-chess
 import sys
+from copy import deepcopy
 input = sys.stdin.readline
 
 # 남 북 동 서
 dx = [0,0,1,-1]
 dy = [1,-1,0,0]
-horsesType = [[],[],[],[],[]]
+horsesType = [[], # 빈 말
+    [[0],[1],[2],[3]], # 1번말
+    [[0,1],[2,3]], # 2번말
+    [[1,2],[0,2],[0,3],[1,3]], # 3번말
+    [[1,2,3],[0,1,2],[0,2,3],[0,1,3]]] # 4번말
 
-def directCnt(x,y,dir):
-    res = 0
+def paddingDir(x,y,dir,tmpMat):
     while True:
         x += dx[dir]
         y += dy[dir]
         if x < 0 or x >= m or y < 0 or y >= n:
             break
-        elif mat[y][x] == 6:
+        elif tmpMat[y][x] == 6:
             break
-        elif mat[y][x] == 0:
-            res += 1
-    return res
+        elif tmpMat[y][x] == 0:
+            tmpMat[y][x] = -1
+
+def dfs(idx, tmpMat):
+    if idx == len(horses):
+        global res
+        tmpRes = 0
+        for i in range(n):
+            tmpRes += tmpMat[i].count(0)
+        res = min(res, tmpRes)
+    else:
+        x, y, type = horses[idx][0], horses[idx][1], horses[idx][2]
+        for ways in horsesType[type]:
+            newMat = deepcopy(tmpMat)
+            for way in ways:
+                paddingDir(x,y,way,newMat)
+            dfs(idx+1, newMat)
 
 if __name__ == "__main__":
     n, m = map(int, input().split())
     mat = []
     horses = []
+    defaultHorse = []
     for i in range(n):
         line = list(map(int ,input().split()))
         for j in range(m):
-            if line[j] in (1,2,3,4,5):
+            if line[j] in (1,2,3,4):
                 horses.append([j,i, line[j]])
+            elif line[j] == 5:
+                defaultHorse.append([j,i,5])
         mat.append(line)
     
-    horses.sort(key = lambda x : (-x[2]))
-
-    for horse in horses:
+    for horse in defaultHorse:
         x, y, type = horse[0], horse[1], horse[2]
-        if type == 1:
-            break
-        elif type == 2:
-            north = directCnt(x,y,1)
-            south = directCnt(x,y,0)
-            west = directCnt(x,y,3)
-            east = directCnt(x,y,2)
-
-
-        elif type == 3:
-            break
-        elif type == 4:
-            break
-        elif type == 5:
-            for i in range(4):
-                nx, ny = x, y
-                while True:
-                    nx += dx[i]
-                    ny += dy[i]
-                    if nx < 0 or nx >= m or ny < 0 or ny >= n:
-                        break
-                    elif mat[ny][nx] == 6:
-                        break
-                    elif mat[ny][nx] == 0:
-                        mat[ny][nx] = -1
+        for i in range(4):
+            paddingDir(x,y,i, mat)
     
-    
+    res = n*m
+    dfs(0,mat)
+    print(res)
